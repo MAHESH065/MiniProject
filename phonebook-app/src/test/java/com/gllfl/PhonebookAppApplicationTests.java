4abf85160b52e1ecf08d3f2ec53d50290a058287
Mahesh <maheshwar.4ever@gmail.com>
2021-08-25 16:12:27 +0530

    change mysql db to h2 db

--- phonebook-app/src/test/java/com/gllfl/PhonebookAppApplicationTests.java
+++ phonebook-app/src/test/java/com/gllfl/PhonebookAppApplicationTests.java
@@ -1,13 +1,10 @@
 package com.gllfl;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PhonebookAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
